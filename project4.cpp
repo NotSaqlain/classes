801cbd0acb3468bfa6d6ee0dfbb6a89c8354f963
notSaqlain <iamnotsaqlain@gmail.com>
2024-03-03 11:46:39 +0100

    add project4

---
+++ project4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class shape {
+private:
+    int len;
+    int width;
+
+public:
+
+    int calculateArea() {
+        return len*width;
+    }
+
+};
+
+class rectangel::shape {
+
+};
+
+int main() {
+    srand(time(NULL));
+
+    return 0;
+}
